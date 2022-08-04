30e98043aab8f9ebdb440e278a2a74a2d6363835
FacundoDevProject <107696659+FacundoDevProject@users.noreply.github.com>
2022-08-03 23:35:41 -0300

    Funcionando 100% backend

---
+++ Project2/BackEnd/src/main/java/com/portfolio/FR2/Fr2Application.java
@@ -0,0 +1,13 @@
+package com.portfolio.FR2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fr2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fr2Application.class, args);
+	}
+
+}
---
+++ Project2/BackEnd/src/test/java/com/portfolio/FR2/Fr2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.FR2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fr2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
